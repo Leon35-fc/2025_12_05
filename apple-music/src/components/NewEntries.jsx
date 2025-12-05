ef9f8a661ec14f825d1942527429fa7c30898678
Leon35-fc <leon.f35@alice.it>
2025-12-05 16:56:52 +0100

    Done. almost

--- apple-music/src/components/NewEntries.jsx
+++ apple-music/src/components/NewEntries.jsx
@@ -1,55 +1,59 @@
-import { useState, useEffect } from "react";
-import { Row, Col, Card } from "react-bootstrap"
-import { FaChevronRight } from "react-icons/fa";
-
+import { useState, useEffect } from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+import { FaChevronRight } from 'react-icons/fa';
 
 const NewEntries = function () {
-    
-    const [newEntries, setNewEntries] = useState([])
-    
-    
-    function fetchNewEntries() {
+  const [newEntries, setNewEntries] = useState([]);
+
+  function fetchNewEntries() {
     fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=tropico')
-        .then((r)=>{ if (r.ok) {
-          return r.json()
+      .then((r) => {
+        if (r.ok) {
+          return r.json();
         } else {
-          throw new Error('Errore nel recupero libri stock')
+          throw new Error('Errore nel recupero libri stock');
         }
-        })
-        .then((songs) => {
-            console.log(songs.data)
-            setNewEntries(songs.data)
-            console.log(newEntries)
-        })
-        .catch((err)=>{
-            console.log("Errore: ", err);
-            
-        }
-        )
-    }
-    
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => {fetchNewEntries()}, [])
+      })
+      .then((songs) => {
+        console.log(songs.data);
+        setNewEntries(songs.data);
+        console.log(newEntries);
+      })
+      .catch((err) => {
+        console.log('Errore: ', err);
+      });
+  }
 
-    return(
-        <>
-            <h4 className='text-light'> Nuove uscite <FaChevronRight className='fs-5 align-middle'/></h4>
-            <Row className="flex-wrap">
-                return (
-                    <Col xs={4} className="p-0">
-                        <Card className="border border-0 bg-transparent text-light">
-                            <Card.Body>
-                            <Card.Img className="round rounded-3 text-info" src='' />
-                            <Card.Text className="fs-6 text-info">
-                                Prologo con Abuelo
-                            </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                )
-            </Row>
-        </>
-    )
-}
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    fetchNewEntries();
+  }, []);
 
-export default NewEntries
+  return (
+    <>
+      <h4 className="text-light">
+        {' '}
+        Nuove uscite <FaChevronRight className="fs-5 align-middle" />
+      </h4>
+      <Row className="flex-wrap">
+        {newEntries.map((song, index) => {
+          return (
+            <Col xs={4} className="p-0" key={index}>
+              <Card className="border border-0 bg-transparent text-light">
+                <Card.Body>
+                  <Card.Img
+                    className="round rounded-3 text-info"
+                    src={song.album.cover_medium}
+                  />
+                  <Card.Text className="fs-6 text-info">{song.title}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
+};
+
+export default NewEntries;
